16baf84bedda3439b843b43359487c236adfb222
Daniel Borba Guimarães da Costa <daniel.borba-@hotmail.com>
2020-06-27 16:45:07 -0300

    Criando a função de converter decimal em hexadecimal

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function decOct() {
 
 function octDec() {
     var octal = document.getElementById("oct").value;
-    var dec = document.getElementById("dec2");
+    let dec = document.getElementById("dec2");
     var erro2 = document.getElementById("erro2")
     var i = 1;
 
@@ -81,4 +81,69 @@ function octDec() {
         res += algarismo*(8**(i-1));
     }
     dec.value = res;
+}
+
+function decHex() {
+    let dec = document.getElementById("dec3").value;
+    let hex = document.getElementById("hex");
+
+    let inteiro = 0;
+    let resto = 0;
+    let letra;
+    let letra2;
+
+    if(dec < 16) {
+        if(dec >= 10) {
+            letra = dec == 10 ? 'A' : 
+                    dec == 11 ? 'B' :
+                    dec == 12 ? 'C' :
+                    dec == 13 ? 'D' :
+                    dec == 14 ? 'E' : 
+                    dec == 15 ? 'F' : '';
+
+            hex.value = letra;
+        }
+        else 
+            hex.value = dec;
+    }
+    if(dec >= 16 && dec <= 159) {
+        inteiro = parseInt(dec/16);
+        resto = dec - inteiro*16;
+
+        letra = resto == 10 ? 'A' : 
+                resto == 11 ? 'B' :
+                resto == 12 ? 'C' :
+                resto == 13 ? 'D' :
+                resto == 14 ? 'E' : 
+                resto == 15 ? 'F' : '';
+
+        if(resto <= 9)
+            hex.value =  inteiro + "" + resto; 
+        else
+            hex.value = inteiro + "" + letra;
+    }
+    if(dec >= 160) {
+        inteiro = parseInt(dec/16);
+        resto = dec - inteiro*16;
+
+        letra = resto == 10 ? 'A' : 
+                resto == 11 ? 'B' :
+                resto == 12 ? 'C' :
+                resto == 13 ? 'D' :
+                resto == 14 ? 'E' : 
+                resto == 15 ? 'F' : '';
+
+        letra2 = inteiro == 10 ? 'A' : 
+                inteiro == 11 ? 'B' :
+                inteiro == 12 ? 'C' :
+                inteiro == 13 ? 'D' :
+                inteiro == 14 ? 'E' : 
+                inteiro == 15 ? 'F' : '';
+
+
+        if(resto <= 9)
+            hex.value =  letra2 + "" + resto; 
+        else
+            hex.value = letra2 + "" + letra;
+    }
 }
